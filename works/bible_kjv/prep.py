707df16ba799734045a66e8cc5673325662e50d7
rsnyder <ron@snyderjr.com>
2016-08-01 16:01:43 -0400

    Initial import of text for bible.

---
+++ works/bible_kjv/prep.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os, sys
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        tokens = line.strip().split('{')[0].split()
+        if len(tokens) >= 3:
+            id = '-'.join(tokens[:2]).lower()
+            text = ' '.join(tokens[2:]).replace('[','<i>').replace(']','</i>')
+            print('<p id="%s">%s</p>\n' % (id, text))
