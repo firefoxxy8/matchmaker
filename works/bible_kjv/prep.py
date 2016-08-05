f333400699b31e6e26118b48518e90fb94c74445
rsnyder <ron@snyderjr.com>
2016-08-05 18:11:49 -0400

    Revised ID tagging for bible-job.

--- works/bible_kjv/prep.py
+++ works/bible_kjv/prep.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     for line in sys.stdin:
         tokens = line.strip().split('{')[0].split()
         if len(tokens) >= 3:
-            chunkId = '-'.join(tokens[:2]).lower().replace(':','-')
+            chunkId = '-'.join(tokens[:2]).lower()
             text = ' '.join(tokens[2:]).replace('[','<i>').replace(']','</i>')
             print('<p id="%s">%s</p>\n' % (chunkId, text))
