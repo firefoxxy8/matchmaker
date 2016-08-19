6e12b8d75ecd009ab3698b9145da8909b767c4d8
rsnyder <ron@snyderjr.com>
2016-08-19 08:30:33 -0400

    Added indexing to bible automation script.

--- works/bible_kjv/process.py
+++ works/bible_kjv/process.py
@@ -24,10 +24,11 @@ if __name__ == '__main__':
         mddir = os.path.join(basedir,'works','bible_kjv','markdown')
         src = os.path.join(rawdir,'%s.txt'%work)
         mdfile = os.path.join(mddir,'%s.md'%work)
-        dest = os.path.join(basedir,'data','%s.json'%work)
+        dest = os.path.join(basedir,'data','bible-%s.json'%work)
         logger.info('basedir=%s work=%s label=%s src=%s mdfile=%s dest=%s process=%s'%(basedir, work, label, src, mdfile, dest, not os.path.exists(dest)))
         emrdir = os.path.join(basedir,'emr')
         if not os.path.exists(dest):
             _execute('echo "# %s" > %s; echo "" >> %s; cat %s | %s >> %s' % (label, mdfile, mdfile, src, os.path.join(srcdir,'prep.py'), mdfile))
             _execute('cd %s; source /opt/python2/bin/activate; python match_quotes_labsemr.py -c matchmaker_mrjob.conf -r emr --no-output -o s3://ithaka-labs/matchmaker/bible_kjv/matches/%s --work %s s3://ithaka-labs/matchmaker/bible_kjv/extracted-quotes/*; cd %s' % (emrdir, work, mdfile, srcdir))
             _execute('mkdir tmp; aws s3 sync s3://ithaka-labs/matchmaker/bible_kjv/matches/%s/ tmp; aws s3 sync s3://ithaka-labs/matchmaker/bible_kjv/matches/%s/ tmp; cat tmp/part-* > %s; rm -rf tmp' % (work, work, dest))
+            _execute('cd ../matchmaker; cat %s | ./metadata.py | ./indexer.py -w bible-%s -v; cd ../emr' % (dest,work))
